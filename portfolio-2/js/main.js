a7c6272144a1bdd859822ab33a55b0e343d8aca6
Randy Tan <sayoonara3344@gmail.com>
2023-06-12 15:29:23 +0700

    complete blog

--- portfolio-2/js/main.js
+++ portfolio-2/js/main.js
@@ -1,1 +1,37 @@
+/* ============== Setting JS like jQuery =============== */
+const $ = document.querySelector.bind(document),
+  $$ = document.querySelectorAll.bind(document);
+
 /* ============== Mixitup Filter =============== */
+var mixer = mixitup(".projects__container", {
+  selectors: {
+    target: ".project__item",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+/* Active Work */
+const linkWork = $$(".category__btn");
+
+function activeWork() {
+  linkWork.forEach((a) => a.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((a) => a.addEventListener("click", activeWork));
+
+/* ============== Testimonials Swiper =============== */
+var testimonialSwiper = new Swiper(".testimonial__container", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
