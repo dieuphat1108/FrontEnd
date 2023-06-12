eeecb4b688a0329a9b7b770ae4905af508d855a4
Randy Tan <sayoonara3344@gmail.com>
2023-06-12 21:36:54 +0700

    complete contact mail

--- portfolio-2/js/main.js
+++ portfolio-2/js/main.js
@@ -35,3 +35,59 @@ var testimonialSwiper = new Swiper(".testimonial__container", {
   mousewheel: true,
   keyboard: true,
 });
+
+/* ============== Contact Form =============== */
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  Message = document.getElementById("message"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Check if the field has a value
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    Message.value === ""
+  ) {
+    // Add and remove color
+    contactMessage.classList.remove("color-light");
+    contactMessage.classList.add("color-dark");
+
+    // Show message
+    contactMessage.textContent = "Write all the input fields";
+  } else {
+    // ServiceID - templateID - #form - publickey
+    emailjs
+      .sendForm(
+        "service_z9ekd6c",
+        "template_en54rai",
+        "#contact-form",
+        "biccaHn8iHWQSH8Rk"
+      )
+      .then(
+        () => {
+          // Show message and add color, window + dot to open emoji
+          contactMessage.classList.add("color-light");
+          contactMessage.textContent = "Message sent ✔";
+
+          // Remove message after 5 seconds
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("OoPs! SOMETHING WENT WRONG ...", error);
+        }
+      );
+
+    // clear input fields
+    contactName.value = "";
+    contactEmail.value = "";
+    Message.value = "";
+  }
+};
+
+contactForm.addEventListener("submit", sendEmail);
