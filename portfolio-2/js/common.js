acb976c9ef8b562969c6705ce9c3442bc5ad9f41
Randy Tan <sayoonara3344@gmail.com>
2023-06-13 10:26:34 +0700

    blog details

---
+++ portfolio-2/js/common.js
@@ -0,0 +1,73 @@
+/* ============== Setting JS like jQuery =============== */
+const $ = document.querySelector.bind(document),
+  $$ = document.querySelectorAll.bind(document);
+
+/* ============== Background Header =============== */
+function scrollHeader() {
+  const header = document.getElementById("header");
+  const activeLink = $$(".nav__link");
+  // when the scroll is greater than 50 viewport height, and the scroll-header class to header tag
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+/* ============== Contact Form =============== */
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  Message = document.getElementById("message"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Check if the field has a value
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    Message.value === ""
+  ) {
+    // Add and remove color
+    contactMessage.classList.remove("color-light");
+    contactMessage.classList.add("color-dark");
+
+    // Show message
+    contactMessage.textContent = "Write all the input fields";
+  } else {
+    // ServiceID - templateID - #form - publickey
+    emailjs
+      .sendForm(
+        "service_z9ekd6c",
+        "template_en54rai",
+        "#contact-form",
+        "biccaHn8iHWQSH8Rk"
+      )
+      .then(
+        () => {
+          // Show message and add color, window + dot to open emoji
+          contactMessage.classList.add("color-light");
+          contactMessage.textContent = "Message sent ✔";
+
+          // Remove message after 5 seconds
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("OoPs! SOMETHING WENT WRONG ...", error);
+        }
+      );
+
+    // clear input fields
+    contactName.value = "";
+    contactEmail.value = "";
+    Message.value = "";
+  }
+};
+
+contactForm.addEventListener("submit", sendEmail);
--- portfolio-2/js/main.js
+++ portfolio-2/js/main.js
@@ -36,58 +36,12 @@ var testimonialSwiper = new Swiper(".testimonial__container", {
   keyboard: true,
 });
 
-/* ============== Contact Form =============== */
-const contactForm = document.getElementById("contact-form"),
-  contactName = document.getElementById("contact-name"),
-  contactEmail = document.getElementById("contact-email"),
-  Message = document.getElementById("message"),
-  contactMessage = document.getElementById("contact-message");
+/* ============== Active Link =============== */
+const navLink = $$(".nav__link");
 
-const sendEmail = (e) => {
-  e.preventDefault();
+function activeLink() {
+  navLink.forEach((a) => a.classList.remove("active-link"));
+  this.classList.add("active-link");
+}
 
-  // Check if the field has a value
-  if (
-    contactName.value === "" ||
-    contactEmail.value === "" ||
-    Message.value === ""
-  ) {
-    // Add and remove color
-    contactMessage.classList.remove("color-light");
-    contactMessage.classList.add("color-dark");
-
-    // Show message
-    contactMessage.textContent = "Write all the input fields";
-  } else {
-    // ServiceID - templateID - #form - publickey
-    emailjs
-      .sendForm(
-        "service_z9ekd6c",
-        "template_en54rai",
-        "#contact-form",
-        "biccaHn8iHWQSH8Rk"
-      )
-      .then(
-        () => {
-          // Show message and add color, window + dot to open emoji
-          contactMessage.classList.add("color-light");
-          contactMessage.textContent = "Message sent ✔";
-
-          // Remove message after 5 seconds
-          setTimeout(() => {
-            contactMessage.textContent = "";
-          }, 5000);
-        },
-        (error) => {
-          alert("OoPs! SOMETHING WENT WRONG ...", error);
-        }
-      );
-
-    // clear input fields
-    contactName.value = "";
-    contactEmail.value = "";
-    Message.value = "";
-  }
-};
-
-contactForm.addEventListener("submit", sendEmail);
+navLink.forEach((a) => a.addEventListener("click", activeLink));
