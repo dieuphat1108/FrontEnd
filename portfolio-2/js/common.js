a3b6ef12f7c7509f165eba0e5a2ed509e2619c92
Randy Tan <sayoonara3344@gmail.com>
2023-06-14 10:46:53 +0700

    complete

--- portfolio-2/js/common.js
+++ portfolio-2/js/common.js
@@ -1,11 +1,39 @@
-/*  ============== Setting JS like jQuery =============== */
-const $ = document.querySelector.bind(document),
-  $$ = document.querySelectorAll.bind(document);
+/* ============== Show Menu =============== */
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* === Menu Show === */
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* === Hide Show === */
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* ============== Remove Menu Mobile =============== */
+const navLinked = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When click on each link, will remove the show menu
+  navMenu.classList.remove("show-menu");
+}
+
+navLinked.forEach((n) => n.addEventListener("click", linkAction));
 
 /* ============== Background Header =============== */
 function scrollHeader() {
   const header = document.getElementById("header");
-  const activeLink = $$(".nav__link");
+  const activeLink = document.querySelectorAll(".nav__link");
   // when the scroll is greater than 50 viewport height, and the scroll-header class to header tag
   if (this.scrollY >= 50) {
     header.classList.add("scroll-header");
@@ -71,3 +99,30 @@ const sendEmail = (e) => {
 };
 
 contactForm.addEventListener("submit", sendEmail);
+
+/* ============== Style Switcher =============== */
+const styleSwitcherToggle = document.querySelector(".style__switcher-toggler"),
+  styleSwitcher = document.querySelector(".style__switcher");
+
+styleSwitcherToggle.addEventListener("click", () => {
+  styleSwitcher.classList.toggle("open");
+});
+
+// Hide switcher on scroll
+window.addEventListener("scroll", () => {
+  if (styleSwitcher.classList.contains("open")) {
+    styleSwitcher.classList.remove("open");
+  }
+});
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
