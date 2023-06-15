05c436cac16ab3bcf1d3b438d3c57d1ef5fe6e00
Randy Tan <sayoonara3344@gmail.com>
2023-06-15 19:39:29 +0700

    setup html layout - css

---
+++ restaurant-website/js/main.js
@@ -0,0 +1,22 @@
+/* ====================== SHOW MENU ========================= */
+
+/* ======= MENU SHOW ======= */
+/* Validate if constant exists */
+
+/* ======= MENU HIDDEN ======= */
+/* Validate if constant exists */
+
+/* ====================== REMOVE MENU MOBILE ========================= */
+
+/* ====================== HOME SWIPER ========================= */
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+/* ====================== DEALS TAB ========================= */
+
+/* ====================== SHOW SCROLL UP ========================= */
